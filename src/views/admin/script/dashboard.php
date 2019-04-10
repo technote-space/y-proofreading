2e33493a3b029e372f8cb90c2e142bdcd375abe9
Technote <technote.space@gmail.com>
2019-04-10 17:37:07 +0900

    #40

---
+++ src/views/admin/script/dashboard.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @version 0.0.7
+ * @author Technote
+ * @since 0.0.7
+ * @copyright Technote All Rights Reserved
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
+ * @link https://technote.space/
+ */
+
+if ( ! defined( 'Y_PROOFREADING' ) ) {
+	return;
+}
+/** @var \WP_Framework_Presenter\Interfaces\Presenter $instance */
+?>
+<script>
+	( function ( $ ) {
+		$( function () {
+			$( '#<?php $instance->id(); ?>-content-wrap option' ).mousedown( function ( e ) {
+				e.preventDefault();
+				$( this ).prop( 'selected', ! $( this ).prop( 'selected' ) );
+				return false;
+			} );
+		} );
+	} )( jQuery );
+</script>

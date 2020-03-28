c8a4a44071791bc05cda016de0129776f5c8da28
Technote <technote.space@gmail.com>
2020-03-29 02:07:49 +0900

    chore: update wp version

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI:
  * Description: Yahoo! API を使用した校正支援プラグイン
  * Author: Technote
- * Version: 0.2.0
+ * Version: next-v0.2.0
  * Author URI: https://technote.space
  * Text Domain: y-proofreading
  * Domain Path: /languages/
